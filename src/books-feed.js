97d1ddfa90bd3cc7cde4ada5055087ff403e3869
Ico <ico@icodavids.nl>
2025-08-07 09:16:09 +0200

    feat: add feed url to feed as 'self'

--- src/books-feed.js
+++ src/books-feed.js
@@ -5,15 +5,17 @@ export default function(books, options) {
         title: options.title,
         id: options.url,
         link: options.url,
+        feed: `https://bieb-feeds.statichost.eu/feeds/${options.filename}.xml`,
         copyright: 'Koninklijke Bibliotheek',
         language: 'nl-NL',
         generator: 'https://github.com/icod/bieb-feeds'
     });
 
     books.forEach(book => {
+
         const item = {
             title: book.title,
-            author: book.authors,
+            author: splitToAuthors(books.authors),
             link: book.url,
             guid: book.url,
             image: book.image,
@@ -25,4 +27,10 @@ export default function(books, options) {
     });
 
     return feed;
+}
+
+const splitToAuthors = function (authorsString) {
+    return authorsString?.split(/[|,;]+/).map(a => a.trim()).map(author => {
+        return {name: author}
+    });
 }
